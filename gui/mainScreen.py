f30549fb46deca315fa247cf36634b0cc3df6ca8
Olivier <olivier.beguinti@gmail.com>
2020-03-20 23:03:06 -0400

    Added info

--- gui/mainScreen.py
+++ gui/mainScreen.py
@@ -7,9 +7,12 @@ class MainScreen:
         self.title = font.render("CALX", True, (0, 0, 0))
         font = pygame.font.Font(None, 26)
         self.undertitle = font.render("The Personal Math Trainer", True, (0, 0, 0))
+        font = pygame.font.Font(None, 16)
+        self.info = font.render("by @olivercrush (github)", True, (0, 0, 0))
 
     def display(self, screen, size):
         screen.fill((245, 245, 245))
         screen.blit(self.title, (size[0] / 2 - self.title.get_width() / 2, 30))
         screen.blit(self.undertitle, (size[0] / 2 - self.undertitle.get_width() / 2, 80))
+        screen.blit(self.info, (size[0] - self.info.get_width() - 5, size[1] - self.info.get_height() - 5))
         return screen
